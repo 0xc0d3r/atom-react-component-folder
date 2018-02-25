2103ea0dd6d1461e42b9ec26d138530fddaae5bd
0xc0d3r <anesh@cybereyelabs.io>
2018-02-25 12:01:45 +0530

    init commit

---
+++ ROADMAP.md
@@ -0,0 +1,5 @@
+### react-component-folder
+
+## Requirements
+
+*
---
+++ spec/react-component-folder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ReactComponentFolderView from '../lib/react-component-folder-view';
+
+describe('ReactComponentFolderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
